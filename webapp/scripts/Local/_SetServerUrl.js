7569116988ac2a747a70be6f200741a8b1fc633a
Paul Vauterin <paul.vauterin@gmail.com>
2013-10-09 16:56:26 +0100

    New server url

--- webapp/scripts/Local/_SetServerUrl.js
+++ webapp/scripts/Local/_SetServerUrl.js
@@ -1,4 +1,5 @@
 //serverUrl = "http://localhost:8000/app01";
 //serverUrl="http://localhost/app";
 //serverUrl = "http://panoptes.cggh.org/sandbox/dqx_server_GenomeTrackViewer/app";
-serverUrl="http://129.67.46.4/DQXServer/app";
+//serverUrl="http://129.67.46.4/DQXServer/app";
+serverUrl = "http://ag1000g-dev.cggh.org//DQXServer/app";
